ad9d14de974dbb30c15a609405782e9652b327dc
Madan <madanpratap2017137@gmail.com>
2025-08-28 05:08:31 +0530

    "fix: deploy.sh"

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -8,23 +8,12 @@ PROJECT_DIR="/home/ubuntu/NatHabit_Twitter_Assignment"
 cd $PROJECT_DIR
 
 
-
+if [ ! -d "venv" ]; then
+  python3 -m venv venv
+fi
 # Activate virtualenv
 source venv/bin/activate
 
-# Check for updates
-echo "Pulling latest code..."
-git fetch origin
-LOCAL_HASH=$(git rev-parse HEAD)
-REMOTE_HASH=$(git rev-parse origin/main)
-
-if [ "$LOCAL_HASH" = "$REMOTE_HASH" ]; then
-    echo "Code is already up-to-date. Skipping deployment."
-    exit 0
-fi
-
-echo "Changes detected. Pulling and updating..."
-git pull origin main
 
 # Install dependencies
 cd backend/
