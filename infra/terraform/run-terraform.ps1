77b0b73f0fb3bb1f40da302c6332482ee71eeecb
Madan <madanpratap2017137@gmail.com>
2025-08-28 15:17:22 +0530

    "fix : removed unused file"

--- infra/terraform/run-terraform.ps1
+++
@@ -1,9 +0,0 @@
-# Load .env from backend folder
-Get-Content "..\..\backend\.env" | ForEach-Object {
-    if ($_ -match "^\s*([^#][^=]+)=(.+)$") {
-        Set-Item -Path "Env:$($matches[1])" -Value "$($matches[2])"
-    }
-}
-
-# Run Terraform
-terraform apply
